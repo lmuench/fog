4b740d19f662ede01310abc7448b6f436576a05d
Ludwig Muench <muench.ludwig@gmail.com>
2018-10-05 18:10:53 +0200

    add cors response filter

---
+++ net.fognode/src/net/fognode/filters/response/cors/Activator.java
@@ -0,0 +1,17 @@
+package net.fognode.filters.response.cors;
+
+import org.apache.felix.dm.DependencyActivatorBase;
+import org.apache.felix.dm.DependencyManager;
+import org.osgi.framework.BundleContext;
+
+public class Activator extends DependencyActivatorBase {
+
+	@Override
+	public void init(BundleContext context, DependencyManager manager) throws Exception {
+		manager.add(
+			createComponent()
+			.setInterface(Object.class.getName(), null)
+			.setImplementation(CorsResponseFilter.class)
+		);
+	}
+}

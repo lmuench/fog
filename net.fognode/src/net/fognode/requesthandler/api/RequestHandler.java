fcfba00630ea94189f860c5440dcc76cfb72601d
Ludwig Muench <muench.ludwig@gmail.com>
2018-11-07 14:16:53 +0100

    add javadoc comments

--- net.fognode/src/net/fognode/requesthandler/api/RequestHandler.java
+++ net.fognode/src/net/fognode/requesthandler/api/RequestHandler.java
@@ -25,12 +25,29 @@ import net.fognode.request.api.Request;
 import net.fognode.response.api.Response;
 
 /**
- * The RequestHandler is the heart of the gateway. It has outgoing dependencies
- * to most of the main components of the gateway.
+ * The RequestHandler is at the heart of the gateway. It has outgoing
+ * dependencies to most of the main components of the gateway:
+ * MappingRepository, Middleware, ShadowFactory and Shadow.
+ * It has an ingoing dependency coming from its associated REST API component
+ * (@see net.fognode.requesthandler.rest.RequestHandlerRest).
+ * It handles every request received by said REST API component in the
+ * following sequence:
+ * 1. Get the URL of the actual resource (e.g. a sensor) from the
+ * MappingRepository (@see net.fognode.mapping.api.MappingRepository) and add  
+ * it to the handled Request (@see net.fognode.request.api.Request) object.
+ * 2. Pass the Request object to all active middleware
+ * (@see net.fognode.middleware.api.Middleware) services.
+ * 3. Pass the Request object to an appropriate device shadow
+ * (@see net.fognode.shadow.api.Shadow) 
  * 
  * @author Ludwig Muench
  *
  */
 public interface RequestHandler {
+	/**
+	 * 
+	 * @param req
+	 * @param res
+	 */
 	public void handleRequest(Request req, Response res);
 }

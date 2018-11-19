60a8f37b2f3d29b0dc933f801171755579848c5d
Ludwig Muench <muench.ludwig@gmail.com>
2018-11-19 22:52:51 +0100

    catch any exception (i.e. IOEception if file doesn't exist) in getEndpoints() and return empy ArrayList

--- net.fognode/src/net/fognode/rd/mock/MockRD.java
+++ net.fognode/src/net/fognode/rd/mock/MockRD.java
@@ -21,7 +21,6 @@
  ******************************************************************************/
 package net.fognode.rd.mock;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -75,31 +74,26 @@ import net.fognode.rd.api.RD;
 public class MockRD implements RD {
 	private String yamlPath;
 	private Yaml yaml;
-	private List<Map<String, Object>> endpoints;
 	
 	public MockRD() {
 		yamlPath = "/net/fognode/rd/mock/endpoints.yaml";
 		yaml = new Yaml();
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<Map<String, Object>> getEndpoints() {
 		try {
-			loadYaml();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return endpoints;
-	}
-	
-	@SuppressWarnings("unchecked")
-	public void loadYaml() throws IOException {
-		endpoints = new ArrayList<Map<String, Object>>();
-		
-		InputStream input = getClass().getResourceAsStream(yamlPath);
-
-		for (Object data : yaml.loadAll(input)) {
-			endpoints.add((Map<String, Object>) data);
+			List<Map<String, Object>> endpoints = new ArrayList<>();
+			
+			InputStream input = getClass().getResourceAsStream(yamlPath);
+			
+			for (Object data : yaml.loadAll(input)) {
+				endpoints.add((Map<String, Object>) data);
+			}
+			return endpoints;
+		} catch (Exception e) {
+			return new ArrayList<Map<String, Object>>();
 		}
 	}
 

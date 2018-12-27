74b9f31d72e07308b7e9c4b2e63281594aa0fb71
Ludwig Muench <muench.ludwig@gmail.com>
2018-12-27 19:51:56 +0100

    delete unused implementation with manual DI. using reflection instead

--- net.fognode/test/net/fognode/mappingrepository/test/persistent/TestablePersistentMappingRepository.java
+++
@@ -1,7 +0,0 @@
-package net.fognode.mappingrepository.test.persistent;
-
-import net.fognode.mappingrepository.persistent.PersistentMappingRepository;
-
-public class TestablePersistentMappingRepository extends PersistentMappingRepository {
-
-}

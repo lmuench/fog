1a297ddf3c7efcb639da4f9f36997ff25d42398b
Ludwig Muench <muench.ludwig@gmail.com>
2018-12-27 17:17:47 +0100

    remove unused import

--- net.fognode/src/net/fognode/request/simple/SimpleRequestFactory.java
+++ net.fognode/src/net/fognode/request/simple/SimpleRequestFactory.java
@@ -21,8 +21,6 @@
  ******************************************************************************/
 package net.fognode.request.simple;
 
-import java.util.Map;
-
 import net.fognode.request.api.Request;
 import net.fognode.request.api.RequestFactory;
 

839b2149d104930f2964346c4a4976e056f27420
Ludwig Muench <muench.ludwig@gmail.com>
2019-01-10 22:05:49 +0100

    add adapter middleware

---
+++ net.fognode/src/net/fognode/middleware/adapter/AdapterMiddleware.java
@@ -0,0 +1,26 @@
+package net.fognode.middleware.adapter;
+
+import java.util.ArrayList;
+
+import net.fognode.middleware.api.Middleware;
+import net.fognode.request.api.Request;
+import net.fognode.response.api.Response;
+
+public class AdapterMiddleware implements Middleware {
+
+	@Override
+	public boolean processRequest(Request req, Response res) {
+		return true;
+	}
+
+	@Override
+	public boolean processResponse(Request req, Response res) {
+		
+		if (res.getStatus() == 200 && res.getPayload() == null) {
+			res.setPayload(new ArrayList<>());
+		}
+		
+		return true;
+	}
+	
+}
